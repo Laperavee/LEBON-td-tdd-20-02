2849221b886c5d5499dca687e46c6402406bab18
Laperavee <tomlebonpro@gmail.com>
2024-02-20 17:07:08 +0100

    Parameters add

---
+++ test/coupon.test.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import Basket from '../src/basket.js';
+describe('Basket Class', function() {
+    let basket;
+
+    beforeEach(() => {
+        basket = new Basket();
+    });
+});
